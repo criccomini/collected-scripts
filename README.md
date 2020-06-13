88e2df6f29d3b8c0fd060fcfd67971a9beb772c8
Chris Riccomini <criccomini@apache.org>
2020-06-12 17:45:26 -0700

    Add post ID to templates. Update README.

--- README.md
+++ README.md
@@ -13,6 +13,35 @@ The scripts require a couple of variables to run. Set your Collected Notes email
 ```
 export COLLECTED_NOTES_API_TOKEN=...
 export COLLECTED_NOTES_API_EMAIL=...
+export COLLECTED_NOTES_SITE_ID=...
 ```
 
 You can find your email and API token [here](https://collectednotes.com/accounts/me/token).
+
+You can find your site ID by editing any post:
+
+```
+https://collectednotes.com/sites/<site ID>/notes/<post ID>/edit
+```
+
+## Templates
+
+The `templates` directory has markdown templates. The templates use Python's built-in [Template](https://docs.python.org/2/library/string.html#template-strings) class. It's simple, but it works. Variables are defined using [PEP 292](https://www.python.org/dev/peps/pep-0292) formatting.
+
+Templates are rendered using the `render_posts.sh` script. This script is specific to my templates and use cases. You should customize it based on your needs.
+
+NOTE: The template filenames must be of the format:
+
+```
+template_name.post_id.md
+```
+
+See the `templates` directory for examples. The post ID is how the `upload_posts.py` script determines which post to update. You must create the notes manually using the Collected Notes UI. Edit the post to see the post ID in the URL.
+
+## Posting
+
+You can upload your rendered templates using the `upload_posts.py` script. This script uses the environment variables from the setup section to upload your posts to Collected Notes.
+
+## About
+
+You might notice some strange design decisions with these scripts. I have gone out of my way not to depend on anything outside packages. No virtual environment is required. No pip installs. Just simply Python and Bash scripts.
--- upload_posts.py
+++ upload_posts.py
@@ -3,23 +3,41 @@
 import httplib
 import json
 import os
+import sys
 
-with open('rendered/lifting_log.md') as f:
-	token = os.environ['COLLECTED_NOTES_API_TOKEN']
-	email = os.environ['COLLECTED_NOTES_API_EMAIL']
-	conn = httplib.HTTPSConnection("collectednotes.com")
-	headers = {
-		'Authorization': '{} {}'.format(email, token),
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-	}
-	json_dict = {
-		"note": 
-		{
-			"body": f.read(), 
-			"visibility": "public_site"
-		}
-	}
-	conn.request("PUT", "/sites/2/notes/3543", json.dumps(json_dict), headers)
-	r1 = conn.getresponse()
-	print r1.status, r1.reason
+COLLECTED_NOTES_SITE_ID = os.environ['COLLECTED_NOTES_SITE_ID']
+COLLECTED_NOTES_API_TOKEN = os.environ['COLLECTED_NOTES_API_TOKEN']
+COLLECTED_NOTES_API_EMAIL = os.environ['COLLECTED_NOTES_API_EMAIL']
+
+def put_file(post_id, markdown):
+  conn = httplib.HTTPSConnection("collectednotes.com")
+  headers = {
+    'Authorization': '{} {}'.format(COLLECTED_NOTES_API_EMAIL, COLLECTED_NOTES_API_TOKEN),
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  json_dict = {
+    "note": 
+    {
+      "body": markdown, 
+      "visibility": "public_site"
+    }
+  }
+  url = '/sites/{}/notes/{}'.format(COLLECTED_NOTES_SITE_ID, post_id)
+  conn.request("PUT", url, json.dumps(json_dict), headers)
+  response = conn.getresponse()
+  print 'post_id={}, URL={}, status={}, response={}'.format(post_id, url, response.status, response.reason)
+
+if __name__=='__main__':
+  if len(sys.argv) < 1:
+    raise ValueError("Missing parameters for [render directory]")
+  
+  render_dir = sys.argv[1]
+
+  for filename in os.listdir(render_dir):
+    file_loc = os.path.join(render_dir, filename)
+
+    if os.path.isfile(file_loc):
+      with open(file_loc) as f:
+        post_id = filename.split('.')[-2]
+        put_file(post_id, f.read())
